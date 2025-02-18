7992ba9f6cd69e93c4bab976c538c8a5814519d8
Çınar Berke ERGUL <c.ergul@jantsa.com.tr>
2025-02-18 15:10:34 +0300

    gateway-server ve port yönlendirme eklendi

---
+++ order-service/src/main/java/com/turkcell/order_service/controllers/OrderController.java
@@ -0,0 +1,15 @@
+package com.turkcell.order_service.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/order")
+public class OrderController {
+    @GetMapping
+    public String get(){
+        return "Order Service.";
+    }
+
+}
