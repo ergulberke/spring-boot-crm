dbfec1617647d3d661fd6bd0b29f79bb0b73e457
Çınar Berke ERGUL <c.ergul@jantsa.com.tr>
2025-02-19 11:43:50 +0300

    rest-client ile sync iletisim

--- order-service/src/main/java/com/turkcell/order_service/controllers/OrderController.java
+++ order-service/src/main/java/com/turkcell/order_service/controllers/OrderController.java
@@ -1,14 +1,27 @@
 package com.turkcell.order_service.controllers;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
 @RestController
 @RequestMapping("/api/v1/order")
 public class OrderController {
+
+    private final RestClient restClient;
+
+    public OrderController(RestClient.Builder restClientBuilder){
+        restClient = restClientBuilder.baseUrl("http://localhost:8085").build();
+    }
+
+
     @GetMapping
     public String get(){
+        String response = restClient.get().uri("/api/v1/product").retrieve().body(String.class);
+        System.out.println("Product Servisten Gelen Cevap: " + response);
+
         return "Order Service.";
     }
 
